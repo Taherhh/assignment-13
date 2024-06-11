2bc397c16f6c4a24184f4ea2caea78e1c29a0d83
Taher Shabbir <taherihh53@gmail.com>
2024-06-10 18:15:32 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//assignment no 13
+var transporttionMode = ["car", "motorcycle", "bicycle", "bus"];
+for (var i = 0; i < transporttionMode.length; i++) {
+    console.log("I would to own a ".concat(transporttionMode[i]));
+}
---
+++ main.ts
@@ -0,0 +1,8 @@
+//assignment no 13
+
+let transporttionMode: string[] = ["car", "motorcycle", "bicycle", "bus"];
+
+for (let i = 0; i < transporttionMode.length; i++) {
+    console.log(`I would to own a ${transporttionMode[i]}` );
+    
+}
